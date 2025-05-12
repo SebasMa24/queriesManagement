2109fa964440060a7ea02883de963dfe18248568
Nicolás Sabogal <mew-four@hotmail.com>
2025-05-12 18:59:37 -0500

    fix: add JsonProperty annotation for password field in User entity to ensure write-only access

--- src/main/java/com/quantumdev/integraservicios/database/model/User.java
+++ src/main/java/com/quantumdev/integraservicios/database/model/User.java
@@ -1,5 +1,7 @@
 package com.quantumdev.integraservicios.database.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -55,6 +57,7 @@ public class User {
     )
     private Role role;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @NotEmpty
     @Column(
         name = "pass_user",
