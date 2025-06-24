25ef8d572b34dea1e13663d0b7f112b547ebfbe7
Nicolás Sabogal <mew-four@hotmail.com>
2025-06-23 19:16:53 -0500

    feat: add WebConfig for CORS configuration with frontend URL support.

--- src/main/java/com/quantumdev/integraservicios/queriesManagement/Config/SecurityConfig.java
+++ src/main/java/com/quantumdev/integraservicios/queriesManagement/Config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import com.quantumdev.integraservicios.database.model.ERole;
 import com.quantumdev.integraservicios.queriesManagement.Jwt.JwtAuthenticationFilter;
@@ -19,9 +20,12 @@ public class SecurityConfig {
     
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
+    private final CorsConfigurationSource corsConfigurationSource;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
                 return http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                     .requestMatchers("/swagger-ui.html").permitAll()
